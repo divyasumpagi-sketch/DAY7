1d285b079d3cf098c8f2dcc808c03485f6735c69
Divya <divyasumpagi@gmail.com>
2026-02-10 22:57:17 +0530

    Convert list to tuple:
    This program converts a list into a tuple and displays the result

---
+++ program3.py
@@ -0,0 +1,8 @@
+# Take input as list
+numbers = list(map(int, input("Enter numbers: ").split()))
+
+# Convert to tuple
+numbers_tuple = tuple(numbers)
+
+# Display result
+print("Tuple:", numbers_tuple)
